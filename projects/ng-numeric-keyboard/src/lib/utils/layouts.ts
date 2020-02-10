f2ed207e02c96b276d3e0f08dd5fbbed88768f2e
Tak <env.tak@gmail.com>
2020-02-10 10:32:33 +0900

    feat: modify layouts and style on keyboard

--- projects/ng-numeric-keyboard/src/lib/utils/layouts.ts
+++ projects/ng-numeric-keyboard/src/lib/utils/layouts.ts
@@ -1,29 +1,5 @@
 import * as Keys from './keys';
 
-const MobileNumberLayout = [
-    [
-        { key: Keys.ONE },
-        { key: Keys.TWO },
-        { key: Keys.THREE },
-        { key: Keys.DEL, },
-    ], [
-        { key: Keys.FOUR },
-        { key: Keys.FIVE },
-        { key: Keys.SIX },
-        { key: Keys.ENTER, },
-    ], [
-        { key: Keys.SEVEN },
-        { key: Keys.EIGHT },
-        { key: Keys.NINE },
-        { key: Keys.DOT },
-    ], [
-        { key: Keys.BLANK },
-        { key: Keys.ZERO },
-        { key: Keys.BLANK },
-        { key: Keys.ESC },
-    ],
-];
-
 const NumberLayout = [
     [
         { key: Keys.ONE },
@@ -66,8 +42,32 @@ const TelLayout = [
     ],
 ];
 
+const PhoneLayout = [
+    [
+        { key: Keys.ONE },
+        { key: Keys.TWO },
+        { key: Keys.THREE },
+        { key: Keys.DEL, },
+    ], [
+        { key: Keys.FOUR },
+        { key: Keys.FIVE },
+        { key: Keys.SIX },
+        { key: Keys.ENTER, },
+    ], [
+        { key: Keys.SEVEN },
+        { key: Keys.EIGHT },
+        { key: Keys.NINE },
+        { key: Keys.DOT },
+    ], [
+        { key: Keys.BLANK },
+        { key: Keys.ZERO },
+        { key: Keys.BLANK },
+        { key: Keys.ESC },
+    ],
+];
+
 export default {
-    mobileNumber: MobileNumberLayout,
     number: NumberLayout,
     tel: TelLayout,
+    phone: PhoneLayout,
 };
