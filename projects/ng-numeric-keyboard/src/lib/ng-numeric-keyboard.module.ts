36e89abf6437af255ce1797b5f19d73f44da7051
Tak <env.tak@gmail.com>
2020-06-08 17:08:20 +0900

    chore: modify version to deploy

--- projects/ng-numeric-keyboard/src/lib/ng-numeric-keyboard.module.ts
+++ projects/ng-numeric-keyboard/src/lib/ng-numeric-keyboard.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { NumericInputModule } from './numeric-input.module';
 
 @NgModule({
-    exports: [
-        NumericInputModule
-    ]
+    exports: [NumericInputModule]
 })
 export class NgNumericKeyboardModule { }
